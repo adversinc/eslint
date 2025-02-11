08d9978a65d546b5b40a854908e1f22135d10370
Dino <dinofabrello@slbiz2life.com>
2025-02-11 14:50:09 +0300

    Add typescript rules

--- eslint.base.config.mjs
+++ eslint.base.config.mjs
@@ -7,7 +7,7 @@ export default [
 		files: ['**/*.ts'],
 		plugins: {
 			jsdoc,
-			tsPlugin,
+			"@typescript-eslint": tsPlugin,
 		},
 
 		languageOptions: {
@@ -18,6 +18,11 @@ export default [
 			"semi": ["error", "always"],
 			"indent": ["error", "tab", {
 				"SwitchCase": 1,
+				"ignoredNodes": [
+					"FunctionExpression > .params[decorators.length > 0]",
+					"FunctionExpression > .params > :matches(Decorator, :not(:first-child))",
+					"ClassBody.body > PropertyDefinition[decorators.length > 0] > .key"
+				]
 			}],
 			"keyword-spacing": ["error", {
 				"overrides": {
@@ -32,7 +37,50 @@ export default [
 			}],
 			"object-curly-spacing": ["error", "always"],
 
+			"space-before-blocks": "error",
 
+			"padding-line-between-statements": [
+				"error",
+				{ "blankLine": "always", "prev": "*", "next": "function" }
+			],
+
+			"lines-around-comment": [
+				"error", {
+					"beforeBlockComment": true,
+					"allowBlockStart": true,
+					"allowObjectStart": true
+				}
+			],
+
+			"operator-linebreak": [
+				"error",
+				"after" // Place the operator before the line break
+			],
+
+			"no-var": "off",
+			"no-constant-condition": "off",
+			"no-case-declarations": "off",
+			"prefer-rest-params": "off",
+
+			// TypeScript rules
+			"@typescript-eslint/indent": [
+				"error", "tab", {
+					"SwitchCase": 1,
+					"ignoredNodes": [
+						"FunctionExpression > .params[decorators.length > 0]",
+						"FunctionExpression > .params > :matches(Decorator, :not(:first-child))",
+						"ClassBody.body > PropertyDefinition[decorators.length > 0] > .key"
+					]
+				}
+			],
+			"@typescript-eslint/no-empty-interface": ["off"],
+			"@typescript-eslint/no-var-requires": ["off"],
+			"@typescript-eslint/no-empty-function": "off",
+			"@typescript-eslint/no-inferrable-types": "off",
+			"@typescript-eslint/ban-ts-comment": "off",
+
+
+			// JSDoc rules
 			"jsdoc/require-jsdoc": [ "error", {
 				"require": {
 					"FunctionDeclaration": true,
